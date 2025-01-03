675a3cf77a2784ea91d1faa9881e4c39771a2353
Andrew Towers <andrew.towers@outlook.com>
2025-01-03 17:16:34 +0000

    added ruff and pyright

--- neovimrc/lua/andrewtowers/lazy/lsp.lua
+++ neovimrc/lua/andrewtowers/lazy/lsp.lua
@@ -1,3 +1,45 @@
+-- Find python path if switch on virtual env
+local function get_ruff_path(workspace)
+  -- Use activated virtualenv.
+  local util = require("lspconfig/util")
+
+  local path = util.path
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, "bin", "ruff")
+  end
+
+  -- Find and use virtualenv via poetry in workspace directory.
+  local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+  if match ~= "" then
+    local venv = vim.fn.trim(vim.fn.system("poetry --directory " .. workspace.. " env info -p"))
+    return path.join(venv, "bin", "ruff")
+  end
+
+  -- Fallback to system ruff.
+  return vim.fn.exepath("ruff")
+end
+
+-- Find python path if switch on virtual env
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  local util = require("lspconfig/util")
+
+  local path = util.path
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+  end
+
+  -- Find and use virtualenv via poetry in workspace directory.
+  local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+  if match ~= "" then
+    local venv = vim.fn.trim(vim.fn.system("poetry --directory " .. workspace.. " env info -e"))
+    return venv
+  end
+
+  -- Fallback to system ruff.
+  return vim.fn.exepath("python3")
+end
+
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -29,12 +71,43 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "ts_ls",
+                "ruff",
+                "pyright",
+                "angularls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
 
                     require("lspconfig")[server_name].setup {
                         capabilities = capabilities
+                    }
+                end,
+
+                ["pyright"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.pyright.setup {
+                        on_new_config = function(config, root_dir)
+                            local pythonPath = get_python_path(root_dir)
+                            print(pythonPath)
+                            config.settings.python.pythonPath = pythonPath
+                        end
+                    }
+                end,
+
+                ["ruff"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.ruff.setup {
+                        capabilities = capabilities,
+                        on_init = function(client)
+                            local ruff_path = get_ruff_path(client.config.root_dir)
+                            client.config.cmd = { ruff_path, 'server'}
+                        end,
+                        init_options = {
+                            settings = {
+                                -- Ruff settings
+                                logLevel = 'debug',
+                            },
+                        }
                     }
                 end,
 
